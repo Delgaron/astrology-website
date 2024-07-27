275c8363f1663d95d661c5b7e15219cdefe441f8
Paulina Drywa <aleksander.szulc03@gmail.com>
2024-07-27 14:29:18 +0200

    enchanced the graphical aspects of the background, it is still not complete but looks better

--- src/StarryNight.js
+++ src/StarryNight.js
@@ -7,7 +7,7 @@ const StarryNight = () => {
       const stars = document.querySelectorAll('.star');
       const scrollY = window.scrollY;
 
-      stars.forEach((star, index) => {
+      stars.forEach((star) => {
         const speed = parseInt(star.dataset.speed);
         const y = (scrollY * speed) / 100;
         star.style.transform = `translateY(${y}px)`;
@@ -23,15 +23,16 @@ const StarryNight = () => {
 
   return (
     <div className="starry-night">
-      {Array.from({ length: 100 }).map((_, index) => (
+      {Array.from({ length: 200 }).map((_, index) => (
         <div
           key={index}
           className="star"
           style={{
             top: `${Math.random() * 100}vh`,
             left: `${Math.random() * 100}vw`,
-            width: `${Math.random() * 3}px`,
-            height: `${Math.random() * 3}px`,
+            width: `${Math.random() * 3 + 1}px`,
+            height: `${Math.random() * 3 + 1}px`,
+            backgroundColor: `rgba(255, 255, 255, ${Math.random()})`,
           }}
           data-speed={Math.random() * 10}
         ></div>
